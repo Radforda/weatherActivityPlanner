e008ac73c526acace2c7df4267d94aa9e587f804
Andrea Radford <radfordandrealynn@gmail.com>
2018-06-18 11:44:25 -0400

    added function which will compare an activity array and a day array and console.log if the activity is recommended

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,15 @@
 
 // all units should be in F mph 
+
+//variables
 var walk={
     name: "Walking",
-    tempMax:85,
+    tempMax:80,
     tempMin:55,
     windMin:0,
     windMax:20,
-    skyCondition:{clear, cloudy},//will need updated depending on the API
-    precip:{none}
+    skyCondition:['clear', 'cloudy'],//will need updated depending on the API
+    precip:['none']
 };
 
 var sail={
@@ -16,59 +18,69 @@ var sail={
     tempMin:60,
     windMin:10,
     windMax:20,
-    skyCondition:[clear, cloudy],//will need updated depending on the API
-    precip:[none, lightRain]
+    skyCondition:['clear', 'cloudy'],//will need updated depending on the API
+    precip:['none', 'lightRain']
 };
 
+//days will be populated with weather API data these are test days currently
+var day1= {
+    name:"Monday",
+    tempMax:70,
+    tempMin:65,
+    windMin:15,
+    windMax:17,
+    skyCondition:"clear",
+    precip:'none'
 
-//We need link to radio buttons and this array this is just a test array
-var selectedActivityArray={walk, sail}
+};
 
-
-//this will be populated with weather API data these are test days currently
-var dayArray=[
-    {
-       name:"Monday",
-       tempMax:110,
-       tempMin:60,
-       windMin:10,
-       windMax:20,
-       skyCondition:{clear, cloudy},//will need updated depending on the API
-       precip:{none, lightRain}
-    },
-    {
+var day2={
     name:"tuesday",
-    tempMax:0,
-    tempMin:-40,
+    tempMax:70,
+    tempMin:60,
     windMin:10,
     windMax:20,
-    skyCondition:{clear, cloudy},//will need updated depending on the API
-    precip:{none, lightRain}
-    }    
-];
+    skyCondition:'clear',
+    precip:'none'
+};
 
+var dayArray=[day1, day2];
+//We need link to radio buttons and this array this is just a test array
+var selectedActivityArray=[walk, sail];
 
+//will be linked to submit button
+checkWeather();
 
-//loop through the activies array and day array
+//loops through the activies array and day array and runs isItAGoodDay function to compare them.
 function checkWeather(){
-    for (var i=0; i<selectedActivityArray.length;i++){
-        for(var j=0; j<dayArray;j++){
-            goodDay(selectedActivityArray[i], dayArray[j]);
+    console.log("check weather is running");
+    console.log("selected activity array length: "+selectedActivityArray.length);
+    console.log("day array length: "+dayArray.length)
+    for (var i=0; i<selectedActivityArray.length; i++){
+        console.log("i: "+i);
+        for(var j=0; j<dayArray.length;j++){
+            console.log("j: "+j);
+            
+            isItAGoodDay(selectedActivityArray[i], dayArray[j]);
         };
     };
 };
 
-//compare activity properties to day properties to determine if the activity is recomended for that day
-function goodDay (activity, day){
-    if (
-    activity.tempMax>day.tempMax && activity.tempMin>day.tempMin
-    && activity.windMax>day.windMax && activity.windMin>day.windMin
-    && activity.skyCondition.indexOf(day.skyCondition)<=0
-    && activity.precip.indexOf(day.precip)<=0){ 
-    
-            return(activity.name, day.name)
-        
-    }
-    
+//compare activity properties to day properties to determine if the activity is recomended for that day.
+//requires two objects as perameters
+function isItAGoodDay (activity, day){
+    console.log( "checking if "+day.name+" is a good day for "+activity.name+".");
+    if (activity.tempMax>day.tempMax && activity.tempMin<day.tempMin){
+        console.log("The temp is good")
+        if(activity.windMax>day.windMax && activity.windMin<day.windMin){
+            console.log("The wind is good")
+            if(activity.skyCondition.indexOf(day.skyCondition)<=0){
+                console.log("the sky condition is good");
+                if(activity.precip.indexOf(day.precip)<=0){
+                    console.log(activity.name+" is recommended for "+day.name);
+                }else{console.log(activity.name+' is not recommended due to '+day.precip)};
+            }else{console.log(activity.name+' is not recommended due to skycondition')};
+        }else{console.log(activity.name+' is not recommended due to wind')};    
+    }else{console.log(activity.name+' is not recommended due to the tempurature')};  
 
 };
