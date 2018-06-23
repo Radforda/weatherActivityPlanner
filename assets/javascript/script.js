36feab865775c23720254d43a484ecbc57823c15
Andrea Radford <radfordandrealynn@gmail.com>
2018-06-23 14:34:19 -0400

    changes from dwayne to update functions to work with object returned from api call

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,23 +4,42 @@ var zipCode ="23235";
 var countryCode="US";
 var forecastDays;
 
+//function to map API response object to application object
+function mapForecastObject(data) {
+    data.list.forEach(element => {
+        var day = {
+            date: moment(element.dt_txt).format('LLL'),
+            name: element.dt_txt,
+            tempMax: (parseFloat(element.main.temp_max - 273.15) * 1.8) + 32,/*Converting temp from Kelvin to Farenheit*/
+            tempMin: (parseFloat(element.main.temp_max - 273.15) * 1.8) + 32,/*Converting temp from Kelvin to Farenheit*/
+            windMin: element.wind.speed,
+            skyCondition: element.weather[0].description,
+        };
+
+        forecastDays.push(day);
+    });
+    console.log(forecastDays);
+}
 
 //Api call to get array of 
-function getForecast(){
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?zip=" + zipCode +","+ countryCode+"&appid=0300e0494f9976a54d845a8f39ccb339";
+function getForecast() {
+    if (zipCode === "") {
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "," + countryCode + "&appid=0300e0494f9976a54d845a8f39ccb339";
+        console.log(zipCode +" "+ cityName);
+    }
+    else {
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?zip=" + zipCode + "," + countryCode + "&appid=0300e0494f9976a54d845a8f39ccb339";
+    }
     $.ajax({
         url: queryURL,
         method: "GET"
-      }).then(function (response) {
+    }).then(function (response) {
         console.log(response);
-        // YOUR CODE GOES HERE!!!
-        forecastDays = response.data;
-    
-        for (var i = 0; i < results.length; i++) {
-
-        }
+        //Calling function to map larger object to usable object
+        mapForecastObject(response);
     });
 }
+
 
 
 getForecast();
@@ -96,39 +115,44 @@ var dayArray=[day1, day2];
 //this will be called inside the api call or to run after the call is complete and the days have been assigned value
 
 
-//loops through the activies array and day array and runs isItAGoodDay function to compare them.
-function checkWeather(){
+
+
+//compare activity properties to day properties to determine if the activity is recomended for that day the console.log it.
+//requires two objects as perameters
+function checkWeather() {
     console.log("check weather is running");
-    console.log("selected activity array length: "+selectedActivityArray.length);
-    console.log("day array length: "+dayArray.length)
-    for (var i=0; i<selectedActivityArray.length; i++){
-        console.log("i: "+i);
-        for(var j=0; j<dayArray.length;j++){
+    console.log("selected activity array length: " + selectedActivityArray.length);
+    console.log("day array length: " + forecastDays.length)
+    for (var i = 0; i < selectedActivityArray.length; i++) {
+        console.log("i: " + i);
+        for (var j = 0; j < forecastDays.length; j++) {
             var activityObject = window[selectedActivityArray[i]];
-            console.log("j: "+j);
-            isItAGoodDay(activityObject, dayArray[j]);
+            console.log("j: " + j);
+            isItAGoodDay(activityObject, forecastDays[j]);
         };
     };
 };
 
 //compare activity properties to day properties to determine if the activity is recomended for that day the console.log it.
 //requires two objects as perameters
-function isItAGoodDay (activity, day){
-    console.log( "checking if "+day.name+" is a good day for "+activity.name+".");
-    if (activity.tempMax>day.tempMax && activity.tempMin<day.tempMin){
+function isItAGoodDay(activity, day) {
+    console.log("checking if " + day.date + " is a good day for " + activity.name + ".");
+    if (activity.tempMax > day.tempMax && activity.tempMin < day.tempMin) {
         console.log("The temp is good")
-        if(activity.windMax>day.windMax && activity.windMin<day.windMin){
+        if (activity.windMax > day.windMin && activity.windMin < day.windMin) {
             console.log("The wind is good")
-            if(activity.skyCondition.indexOf(day.skyCondition)<=0){
+            if (activity.skyCondition.indexOf(day.skyCondition) <= 0) {
                 console.log("the sky condition is good");
-                if(activity.precip.indexOf(day.precip)<=0){
-                    console.log(activity.name+" is recommended for "+day.name);
-                }else{console.log(activity.name+' is not recommended due to '+day.precip)};
-            }else{console.log(activity.name+' is not recommended due to skycondition')};
-        }else{console.log(activity.name+' is not recommended due to wind')};    
-    }else{console.log(activity.name+' is not recommended due to the tempurature')};  
+                if (activity.precip.indexOf(day.precip) <= 0) {
+                    console.log(activity.name + " is recommended for " + day.name);
+                } else { console.log(activity.name + ' is not recommended due to ' + day.precip) };
+            } else { console.log(activity.name + ' is not recommended due to skycondition') };
+        } else { console.log(activity.name + ' is not recommended due to wind') };
+    } else { console.log(activity.name + ' is not recommended due to the tempurature') };
 
 };
+
+
 
 
 //firebase config
